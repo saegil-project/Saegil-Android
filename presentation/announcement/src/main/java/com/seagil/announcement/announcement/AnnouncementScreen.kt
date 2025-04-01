4bac75bacf96f730cc4f5bdc667113bc3014ac97
minjun011026 <jun011026@gmail.com>
2025-04-01 17:11:11 +0900

    fix: file staging

--- presentation/announcement/src/main/java/com/seagil/announcement/announcement/AnnouncementScreen.kt
+++ presentation/announcement/src/main/java/com/seagil/announcement/announcement/AnnouncementScreen.kt
@@ -2,19 +2,21 @@ package com.seagil.announcement.announcement
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import com.saegil.designsystem.theme.h1
 
 @Composable
 fun AnnouncementScreen(
 ) {
     // TODO UI Rendering
     Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text(text = "공지 화면", fontSize = 24.sp, style =)
+        Text(text = "공지 화면", fontSize = 24.sp, style = MaterialTheme.typography.h1)
     }
 }
 
