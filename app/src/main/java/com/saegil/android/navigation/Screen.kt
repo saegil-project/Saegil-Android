63d80699089bff6c07f824eee8db953fd772e7e5
Yu Jin <qldls0307@naver.com>
2025-04-01 21:37:44 +0900

    feat: BottomNavigationBar Design 반영 및 ic 파일 추가

--- app/src/main/java/com/saegil/android/navigation/Screen.kt
+++ app/src/main/java/com/saegil/android/navigation/Screen.kt
@@ -4,10 +4,10 @@ import androidx.annotation.DrawableRes
 import com.saegil.android.R
 
 sealed class Screen(val route: String, @DrawableRes val icon: Int, val label: String) {
-    object Learning : Screen("learning", R.drawable.ic_launcher_foreground, "학습")
-    object Announcement : Screen("announcement", R.drawable.ic_launcher_foreground, "공지사항")
-    object Map : Screen("map", R.drawable.ic_launcher_foreground, "지도")
-    object MyPage : Screen("mypage", R.drawable.ic_launcher_foreground, "마이페이지")
+    object Learning : Screen("learning", R.drawable.ic_pencil, "학습")
+    object Announcement : Screen("announcement", R.drawable.ic_announcement, "공지사항")
+    object Map : Screen("map", R.drawable.ic_location, "지도")
+    object MyPage : Screen("mypage", R.drawable.ic_user_circle, "마이페이지")
 
     companion object {
         val items = listOf(Learning, Announcement, Map, MyPage)
--- app/src/main/java/com/saegil/android/navigation/ui/BottomNavigationBar.kt
+++ app/src/main/java/com/saegil/android/navigation/ui/BottomNavigationBar.kt
@@ -1,40 +1,78 @@
 package com.saegil.android.navigation.ui
 
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.saegil.android.navigation.Screen
+import com.saegil.designsystem.theme.BackgroundGray
+import com.saegil.designsystem.theme.MainYellow
+import com.saegil.designsystem.theme.TextGray
 
 @Composable
 fun BottomNavigationBar(navController: NavHostController) {
     val currentBackStackEntry by navController.currentBackStackEntryAsState()
     val currentDestination = currentBackStackEntry?.destination?.route
 
-    NavigationBar {
-        Screen.items.forEach { screen ->
-            NavigationBarItem(
-                icon = {
-                    Icon(
-                        painterResource(id = screen.icon),
-                        contentDescription = screen.label
+    Surface(
+        shadowElevation = 8.dp,
+        color = TextGray,
+    ) {
+        NavigationBar(
+            tonalElevation = 0.dp,
+            containerColor = BackgroundGray
+        ) {
+            Screen.items.forEach { screen ->
+                NavigationBarItem(
+                    icon = {
+                        Icon(
+                            painter = painterResource(id = screen.icon),
+                            contentDescription = screen.label,
+                        )
+                    },
+                    label = {
+                        Text(
+                            text = screen.label,
+                        )
+                    },
+                    selected = currentDestination == screen.route,
+                    onClick = {
+                        navController.navigate(screen.route) {
+                            popUpTo(navController.graph.startDestinationId) { saveState = true }
+                            launchSingleTop = true
+                            restoreState = true
+                        }
+                    },
+                    colors = NavigationBarItemDefaults.colors(
+                        selectedIconColor = MainYellow,
+                        selectedTextColor = MainYellow,
+                        indicatorColor = BackgroundGray
                     )
-                },
-                label = { Text(screen.label) },
-                selected = currentDestination == screen.route,
-                onClick = {
-                    navController.navigate(screen.route) {
-                        popUpTo(navController.graph.startDestinationId) { saveState = true }
-                        launchSingleTop = true
-                        restoreState = true
-                    }
-                }
-            )
+                )
+            }
         }
+    }
+}
+
+
+@Composable
+@Preview(showBackground = true)
+fun PreviewBottomNavigationBar() {
+    val navController = rememberNavController()
+
+    // 테마 적용을 위해 MaterialTheme으로 감싸기
+    MaterialTheme {
+        BottomNavigationBar(navController = navController)
     }
 }
