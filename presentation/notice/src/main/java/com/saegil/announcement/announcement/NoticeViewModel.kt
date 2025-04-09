b62ddfabd8e50ecc072a219a3138d143eaa3f1a1
minjun011026 <jun011026@gmail.com>
2025-04-09 16:39:02 +0900

    rename: announcementtonotice

--- presentation/notice/src/main/java/com/saegil/announcement/announcement/NoticeViewModel.kt
+++ presentation/notice/src/main/java/com/saegil/announcement/announcement/NoticeViewModel.kt
@@ -10,12 +10,12 @@ import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
-import com.saegil.announcement.announcement.AnnouncementUiState.*
+import com.saegil.announcement.announcement.NoticeUiState.*
 import com.saegil.domain.model.Notice
 import com.saegil.domain.usecase.GetFeedUseCase
 
 @HiltViewModel
-class AnnouncementViewModel @Inject constructor(
+class NoticeViewModel @Inject constructor(
     getFeedUseCase: GetFeedUseCase
 ) : ViewModel() {
 
@@ -27,9 +27,9 @@ class AnnouncementViewModel @Inject constructor(
                 initialValue = PagingData.empty()
             )
 
-    val feedUiState: StateFlow<AnnouncementUiState> =
+    val feedUiState: StateFlow<NoticeUiState> =
         getFeedUseCase()
-            .map<PagingData<Notice>, AnnouncementUiState> { Success }
+            .map<PagingData<Notice>, NoticeUiState> { Success }
             .onStart { emit(Loading) }
             .stateIn(
                 scope = viewModelScope,
