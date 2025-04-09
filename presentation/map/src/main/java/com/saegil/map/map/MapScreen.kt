41e0cd04cbc2f33d083912e9c51e7e46131825ac
Yu Jin <qldls0307@naver.com>
2025-04-09 18:49:47 +0900

    feat: presentation 계층 Map Organization api 작업

--- presentation/map/src/main/java/com/saegil/map/map/MapScreen.kt
+++ presentation/map/src/main/java/com/saegil/map/map/MapScreen.kt
@@ -2,18 +2,47 @@ package com.saegil.map.map
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.naver.maps.map.compose.ExperimentalNaverMapApi
 import com.naver.maps.map.compose.NaverMap
 
 
-@OptIn(ExperimentalNaverMapApi::class)
 @Composable
 fun MapScreen(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    viewModel: MapViewModel = hiltViewModel(),
 ) {
 
+    val mapState by viewModel.mapUiState.collectAsStateWithLifecycle()
+
+    MapScreen(
+        mapState,
+        modifier
+    )
+
+    when (mapState) {
+        MapUiState.Loading -> {}
+        is MapUiState.Success -> {
+
+        }
+    }
+
+
+//    viewModel.getNearByOriganizations( 37.5326, 126.8469,1000)
+
+}
+
+@OptIn(ExperimentalNaverMapApi::class)
+@Composable
+internal fun MapScreen(
+    state: MapUiState,
+//    actions: MapActions,
+    modifier: Modifier = Modifier,
+) {
     NaverMap(
         modifier = Modifier.fillMaxSize()
     )
--- presentation/map/src/main/java/com/saegil/map/map/MapViewModel.kt
+++ presentation/map/src/main/java/com/saegil/map/map/MapViewModel.kt
@@ -1,23 +1,68 @@
 package com.saegil.map.map
 
-import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.saegil.domain.model.Organization
+import com.saegil.domain.usecase.GetMapListUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
 class MapViewModel @Inject constructor(
-    savedStateHandle: SavedStateHandle
+    getMapListUseCase: GetMapListUseCase,
 ) : ViewModel() {
 
-    private val _stateFlow: MutableStateFlow<MapState> = MutableStateFlow(MapState())
+    //    private val _mapState: MutableStateFlow<MapState> = MutableStateFlow(MapState())
+//    val mapState: StateFlow<MapState> = _mapState.asStateFlow()
+//
+//    private val _actionsFlow: MutableStateFlow<MapActions> = MutableStateFlow(MapActions())
+//    val actionsFlow: StateFlow<MapActions> = _actionsFlow.asStateFlow()
+//
+//    suspend fun getNearByOriganizations(
+//        latitude: Double?,
+//        longitude: Double?,
+//        radius: Int?,
+//    ) {
+//
+//        getMapListUseCase(
+//            latitude,
+//            longitude,
+//            radius
+//        ).collect{
+//            _mapState.value = _mapState.value
+//
+//        }
+//
+//    }
+    val mapUiState: StateFlow<MapUiState> =
+        getMapListUseCase(
+            37.5326, 126.8469, 1000
+        )
+            .map<List<Organization>, MapUiState>(MapUiState::Success)
+//            .onStart { emit(Loading) }
+            .stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(5_000),
+                initialValue = MapUiState.Loading,
+            )
 
-    val stateFlow: StateFlow<MapState> = _stateFlow.asStateFlow()
+}
+
+class MapActions {
 
 
 }
 
-class MapState
+sealed interface MapUiState {
+
+    data object Loading : MapUiState
+
+    data class Success(
+        val orignizations: List<Organization>
+    ) : MapUiState
+
+}
