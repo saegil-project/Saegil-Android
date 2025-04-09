7e5db4ae6bd07f24f8ac381e0e1f8d9e4aaf99ab
Yu Jin <qldls0307@naver.com>
2025-04-09 18:49:38 +0900

    feat: domain 계층 Map Organization api 작업

---
+++ domain/src/main/java/com/saegil/domain/model/Organization.kt
@@ -0,0 +1,12 @@
+package com.saegil.domain.model
+
+data class Organization(
+    val id: Long,
+    val name: String,
+    val latitude: Double,
+    val longitude: Double,
+    val operatingHours: String,
+    val telephoneNumber: String,
+    val address: String,
+    val distance: Double,
+)
