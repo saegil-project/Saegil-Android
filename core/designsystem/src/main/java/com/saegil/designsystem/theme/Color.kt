7ade001b4736cc614ffedda4ff218d239011323d
Yu Jin <qldls0307@naver.com>
2025-04-14 00:58:48 +0900

    chore: lightGray 추가

--- core/designsystem/src/main/java/com/saegil/designsystem/theme/Color.kt
+++ core/designsystem/src/main/java/com/saegil/designsystem/theme/Color.kt
@@ -3,9 +3,10 @@ package com.saegil.designsystem.theme
 import androidx.compose.ui.graphics.Color
 
 val PrimaryBlue = Color(0xFF3384E4)
-val Blue = Color(0xFFE7F2FF)
 val AccentYellow = Color(0xFFFFC107)
 val BackgroundGray = Color(0xFFF5F5F5)
+val BackgroundBlue = Color(0xFFE7F2FF)
 val TextBlack = Color(0xFF212121)
 val TextGray = Color(0xFF767676)
 val DisabledGray = Color(0xFFB0BEC5)
+val lightGray = Color(0xFFF2F2F2)
