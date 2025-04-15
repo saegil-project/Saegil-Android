6e029cec0473b8d475fc131b324b0248bd331f12
Yu Jin <qldls0307@naver.com>
2025-04-16 02:01:51 +0900

    chore: Http 통신 Logger 추가

--- data/src/main/java/com/saegil/data/di/NetworkModule.kt
+++ data/src/main/java/com/saegil/data/di/NetworkModule.kt
@@ -12,7 +12,9 @@ import io.ktor.client.HttpClient
 import io.ktor.client.engine.android.Android
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.plugins.logging.SIMPLE
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 import javax.inject.Singleton
@@ -27,6 +29,7 @@ object NetworkModule {
         return HttpClient(Android) {
             install(Logging) {
                 level = LogLevel.ALL
+                logger = Logger.SIMPLE
             }
             install(ContentNegotiation) {
                 json(Json { ignoreUnknownKeys = true })
