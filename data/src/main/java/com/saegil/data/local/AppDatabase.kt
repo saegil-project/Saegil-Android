02228e8b43e62f402938c2f2418efe9c1b091c07
minjun011026 <jun011026@gmail.com>
2025-04-19 03:24:05 +0900

    feat: exportscheme-to-false

--- data/src/main/java/com/saegil/data/local/AppDatabase.kt
+++ data/src/main/java/com/saegil/data/local/AppDatabase.kt
@@ -4,7 +4,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.saegil.data.model.TokenEntityDto
 
-@Database(entities = [TokenEntityDto::class], version = 1)
+@Database(entities = [TokenEntityDto::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun tokenDao(): TokenDao
