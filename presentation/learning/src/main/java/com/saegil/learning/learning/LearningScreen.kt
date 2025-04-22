8d5eb584bb99676f5220ff97e06744276180c2b5
Yu Jin <qldls0307@naver.com>
2025-04-22 17:05:14 +0900

    feat: 캐릭터 표정 이미지 enum으로 관리

--- presentation/learning/src/main/java/com/saegil/learning/learning/LearningScreen.kt
+++ presentation/learning/src/main/java/com/saegil/learning/learning/LearningScreen.kt
@@ -3,18 +3,15 @@ package com.saegil.learning.learning
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -78,7 +75,7 @@ fun SaegilCharacter(
 ) {
     Image(
         modifier= modifier.size(134.dp),
-        painter = painterResource(id = R.drawable.img_normal),
+        painter = painterResource(id = charactorEmotion.img),
         contentDescription = "새길 캐릭터 이미지"
     )
 }
--- presentation/learning/src/main/java/com/saegil/learning/learning/SaegilCharactor.kt
+++ presentation/learning/src/main/java/com/saegil/learning/learning/SaegilCharactor.kt
@@ -2,7 +2,7 @@ package com.saegil.learning.learning
 
 import com.saegil.learn.R
 
-enum class SaegilCharactor(img: Int) {
+enum class SaegilCharactor(val img: Int) {
     NORMAL(R.drawable.img_normal),
     SMILE(R.drawable.img_smile),
     SAYING(R.drawable.img_saying),
