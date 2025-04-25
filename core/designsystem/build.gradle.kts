f53c761d3658e436087741e46cebb5a7b226bf1d
minjun011026 <jun011026@gmail.com>
2025-04-26 00:13:17 +0900

    remove: unused-library

--- core/designsystem/build.gradle.kts
+++ core/designsystem/build.gradle.kts
@@ -44,8 +44,6 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.androidx.runtime.android)
-    implementation(libs.androidx.browser)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
