438118cd6e00a1ea0e73b4402b8d67b1e89cf679
minjun011026 <jun011026@gmail.com>
2025-05-01 22:38:06 +0900

    feat: scenarioentity

--- data/src/main/java/com/saegil/data/local/AppDatabase.kt
+++ data/src/main/java/com/saegil/data/local/AppDatabase.kt
@@ -1,6 +1,10 @@
 package com.saegil.data.local
 
+import androidx.room.Database
 import androidx.room.RoomDatabase
 
+@Database(entities = ScenarioDto)
 abstract class AppDatabase : RoomDatabase() {
+
+
 }
