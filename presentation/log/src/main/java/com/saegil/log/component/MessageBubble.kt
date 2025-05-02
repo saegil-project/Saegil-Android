0e9d860dab20d179d4faf99d5de38cd14b4590e7
minjun011026 <jun011026@gmail.com>
2025-05-02 20:47:58 +0900

    feat: messagebubble-padding

--- presentation/log/src/main/java/com/saegil/log/component/MessageBubble.kt
+++ presentation/log/src/main/java/com/saegil/log/component/MessageBubble.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.saegil.designsystem.theme.SaegilAndroidTheme
 import com.saegil.designsystem.theme.caption
 
 @Composable
@@ -19,15 +20,20 @@ fun MessageBubble(
 ) {
     Surface(
         shape = RoundedCornerShape(4.dp),
-        color = if(isUser) MaterialTheme.colorScheme.secondary else MaterialTheme.colorScheme.onSecondary,
-        modifier = modifier.padding(
-            end = 40.dp
-        )
-    ){
+        color = if (isUser) MaterialTheme.colorScheme.secondary else MaterialTheme.colorScheme.onSecondary,
+        modifier = modifier
+            .padding(
+                start = if (isUser) 64.dp else 8.dp,
+                end = if (isUser) 8.dp else 64.dp,
+                top = 4.dp,
+                bottom = 4.dp
+            )
+    ) {
         Text(
             text = message,
             modifier = Modifier.padding(12.dp),
-            style = MaterialTheme.typography.caption
+            style = MaterialTheme.typography.caption,
+            color = MaterialTheme.colorScheme.onSurface,
         )
     }
 }
@@ -35,17 +41,21 @@ fun MessageBubble(
 @Preview(apiLevel = 33)
 @Composable
 fun UserMessageBubblePreview() {
-    MessageBubble(
-        isUser = true,
-        message = "라면은 어디에 있습니까?"
-    )
+    SaegilAndroidTheme {
+        MessageBubble(
+            isUser = true,
+            message = "라면은 어디에 있습니까?"
+        )
+    }
 }
 
 @Preview(apiLevel = 33)
 @Composable
 fun AiMessageBubblePreview() {
-    MessageBubble(
-        isUser = false,
-        message = "방금의 대화에서 ‘꼬부랑 국수’는 북한에서 사용하는 단어이며, 한국어로 ‘라면’이라고 부릅니다. 꼬부랑 국수 대신 라면을 활용하여 다시 말씀해보세요."
-    )
+    SaegilAndroidTheme {
+        MessageBubble(
+            isUser = false,
+            message = "방금의 대화에서 ‘꼬부랑 국수’는 북한에서 사용하는 단어이며, 한국어로 ‘라면’이라고 부릅니다. 꼬부랑 국수 대신 라면을 활용하여 다시 말씀해보세요."
+        )
+    }
 }
