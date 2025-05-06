6809ee05c3afc04675165cbf240e508ed2e1f3c6
minjun011026 <jun011026@gmail.com>
2025-05-07 00:03:15 +0900

    feat: scenariotitem-date-string

--- core/designsystem/src/main/java/com/saegil/designsystem/component/ScenarioItem.kt
+++ core/designsystem/src/main/java/com/saegil/designsystem/component/ScenarioItem.kt
@@ -1,6 +1,7 @@
 package com.saegil.designsystem.component
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -17,17 +18,21 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
 import coil3.request.ImageRequest
 import coil3.request.crossfade
+import com.saegil.designsystem.theme.SaegilAndroidTheme
+import com.saegil.designsystem.theme.caption
 import com.saegil.designsystem.theme.h3
 
 @Composable
 fun ScenarioItem(
     name: String,
     iconImageUrl: String,
-    onClick: () -> Unit
+    onClick: () -> Unit,
+    date: String? = null,
 ) {
     Card(
         modifier = Modifier
@@ -36,8 +41,7 @@ fun ScenarioItem(
             .clickable { onClick() },
         colors = CardDefaults.cardColors(
             containerColor = MaterialTheme.colorScheme.primaryContainer,
-
-            ),
+        ),
         elevation = CardDefaults.cardElevation(
             defaultElevation = 4.dp
         )
@@ -45,11 +49,20 @@ fun ScenarioItem(
         Row(
             modifier = Modifier.padding(top = 16.dp, start = 20.dp, end = 18.dp, bottom = 16.dp)
         ) {
-            Text(
-                text = name,
-                style = MaterialTheme.typography.h3,
-                color = MaterialTheme.colorScheme.onSurface,
-            )
+            Column {
+                Text(
+                    text = name,
+                    style = MaterialTheme.typography.h3,
+                    color = MaterialTheme.colorScheme.onSurface,
+                )
+                date?.let {
+                    Text(
+                        text = date,
+                        style = MaterialTheme.typography.caption,
+                        modifier = Modifier.padding(top = 10.dp)
+                    )
+                }
+            }
 
             Spacer(modifier = Modifier.weight(1f))
 
@@ -67,5 +80,18 @@ fun ScenarioItem(
                     .clip(RoundedCornerShape(8.dp))
             )
         }
+    }
+}
+
+@Preview(apiLevel = 33)
+@Composable
+fun ScenarioItemPreview() {
+    SaegilAndroidTheme {
+        ScenarioItem(
+            name = "밥 먹을 때",
+            iconImageUrl = "3",
+            onClick = {},
+            date = "1111111"
+        )
     }
 }
