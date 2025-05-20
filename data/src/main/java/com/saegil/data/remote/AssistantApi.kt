834e7ffe3c6baba5ae1e8fbf8d09cc64e83bc068
Yu Jin <qldls0307@naver.com>
2025-05-20 16:56:27 +0900

    feat: threadId 유무에 따라 오버로딩으로 다형성 구현

--- data/src/main/java/com/saegil/data/remote/AssistantApi.kt
+++ data/src/main/java/com/saegil/data/remote/AssistantApi.kt
@@ -6,11 +6,13 @@ import retrofit2.Response
 import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.Part
+import retrofit2.http.Query
 
 interface AssistantApi { //todo ktor로 마이그레이션 하기
     @Multipart
     @POST("/api/v1/llm/assistant/upload")
     suspend fun uploadAudio(
-        @Part file: MultipartBody.Part
+        @Part file: MultipartBody.Part,
+        @Query("thread_id") threadId: String? = null
     ): Response<UploadAudioDto>
 }
